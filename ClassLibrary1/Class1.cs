a164b52d59de27c1151bf79ba8b0fd0a0a9b5a22
bjarnesvanberg <bjsv@easj.dk>
2014-08-26 13:19:58 +0200

    Solution and projects added

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Person
+    {
+        private string _name;
+        private int _age;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public Person()
+        {}
+
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        public bool isAdult()
+        {
+            return _age > 17;
+        }
+    } // End of class
+} // end of namespace
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+//using ClassLibrary1;
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTestProject1
+{
+    
+
+    [TestClass]
+    public class UnitTest1
+    {
+        private ClassLibrary1.Person _p;
+
+        [TestInitialize]
+        public void BeginTest()
+        {
+            _p = new Person();
+            _p.Name = "Magne";
+            _p.Age = 4;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bool result = _p.isAdult();
+            Assert.IsTrue(result);
+        }
+    }
+}
