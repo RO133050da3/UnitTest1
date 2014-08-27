00122725edfee54e61468a101b8c4841872a1b7d
bjarnesvanberg <bjsv@easj.dk>
2014-08-27 07:44:31 +0200

    Implemented Equals function

--- ClassLibrary1/Person.cs
+++ ClassLibrary1/Person.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    public class Person 
+    public class Person : IEquatable<Person>
     {
         private string _name;
         private decimal _age;
-
+        private string _ssn; // Social Security Number
         public string Name
         {
             get { return _name; }
@@ -28,13 +28,24 @@ namespace ClassLibrary1
             set { _age = value; }
         }
 
+        public string SSN
+        {
+            get { return _ssn; }
+            set { _ssn = value; }
+        }
 
         public Person()
         {}
-        
-        public Person(string name)
+
+        public Person(string ssn, string name)
         {
+            this.SSN = ssn;
             this.Name = name;
+        }
+        
+        public Person(string ssn)
+        {
+            this.SSN = ssn;
         }
 
         public bool isAdult()
@@ -42,5 +53,17 @@ namespace ClassLibrary1
             return _age >= 18;
         }
 
+        public bool Equals(Person other)
+        {
+            if (this.SSN != other.SSN)
+                return false;
+            if (this.Name != other.Name)
+                return false;
+            if (this.Age != other.Age)
+                return false;
+
+            // In the case that all of the above tests are false
+            return true;
+        }
     } 
 }
